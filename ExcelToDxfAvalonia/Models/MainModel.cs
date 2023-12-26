90a1ded66a71c467f30c355b5da8554f6cd57a43
Yury Golub <yuragolub21@gmail.com>
2023-12-26 16:16:18 +0300

    Implement excel file reading and console visibility switching.

--- ExcelToDxfAvalonia/Models/MainModel.cs
+++ ExcelToDxfAvalonia/Models/MainModel.cs
@@ -1,5 +1,58 @@
-﻿namespace ExcelToDxfAvalonia.Models;
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using ExcelDataReader;
+using ExcelToDxfAvalonia.Extensions;
+
+namespace ExcelToDxfAvalonia.Models;
 
 public class MainModel
 {
+    public static void SwitchConsoleVisibility()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            if (ConsoleManager.IsConsoleVisible())
+            {
+                ConsoleManager.HideConsole();
+            }
+            else
+            {
+                ConsoleManager.ShowConsole();
+            }
+        }
+    }
+
+    public void ReadExcelFile(string filePath)
+    {
+        using FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        // Auto-detect format, supports:
+        //  - Binary Excel files (2.0-2003 format; *.xls)
+        //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
+        using IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
+
+        DataSet result = reader.AsDataSet();
+
+        Console.OutputEncoding = Encoding.UTF8;
+
+        foreach (DataTable table in result.Tables)
+        {
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write($"{item}  ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("====================");
+        }
+    }
 }
--- ExcelToDxfAvalonia/ViewModels/MainViewModel.cs
+++ ExcelToDxfAvalonia/ViewModels/MainViewModel.cs
@@ -1,18 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Platform.Storage;
 using ExcelToDxfAvalonia.Models;
 
 namespace ExcelToDxfAvalonia.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    private readonly MainModel mainModel;
+    private readonly MainModel model;
 
-    public MainViewModel(MainModel mainModel)
+    private string inFilePath;
+
+    public MainViewModel(MainModel model)
     {
-        this.mainModel = mainModel ?? throw new ArgumentNullException(nameof(mainModel));
+        this.model = model ?? throw new ArgumentNullException(nameof(model));
     }
 
-    public void TestButton()
+    public string FileName { get; set; }
+
+    public void SwitchConsoleVisibility() => MainModel.SwitchConsoleVisibility();
+
+    public async Task OpenFileAsync()
     {
+        var filters = new List<FilePickerFileType>
+        {
+            new FilePickerFileType("Excel file (*.xlsx)")
+            {
+                Patterns = new List<string> { "*.xlsx" },
+            },
+        };
+
+        if (!await this.OpenFileAsync("Open CanHelper log file", filters))
+        {
+            return;
+        }
+
+        this.model.ReadExcelFile(this.inFilePath);
+    }
+
+    private async Task<bool> OpenFileAsync(string title, List<FilePickerFileType> filters)
+    {
+        filters.Add(new FilePickerFileType("All files (*.*)")
+        {
+            Patterns = new List<string> { "*.*" },
+        });
+
+        IReadOnlyList<IStorageFile> files = null;
+        if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            TopLevel topLevel = TopLevel.GetTopLevel(desktop.MainWindow);
+
+            files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = title,
+                AllowMultiple = false,
+                FileTypeFilter = filters,
+            });
+        }
+
+        if (files?.Count > 0)
+        {
+            string file = files[0].Path.LocalPath;
+
+            this.inFilePath = file;
+            this.FileName = Path.GetFileName(file);
+            return true;
+        }
+
+        return false;
     }
 }
