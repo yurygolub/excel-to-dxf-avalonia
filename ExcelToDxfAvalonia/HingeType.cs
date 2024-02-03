6476b2d5a1a7d50e61c4dbe29e38436d370ec2fc
Yury Golub <yuragolub21@gmail.com>
2024-02-03 21:22:45 +0300

    Fix display of hinge type.

--- ExcelToDxfAvalonia/HingeType.cs
+++ ExcelToDxfAvalonia/HingeType.cs
@@ -1,9 +1,18 @@
-﻿namespace ExcelToDxfAvalonia;
+﻿using System.ComponentModel;
+
+namespace ExcelToDxfAvalonia;
 
 public enum HingeType
 {
+    [Description("EB 755")]
     HingeEB_755,
+
+    [Description("R-10 102x76")]
     HingeR_10_102x76,
+
+    [Description("4BB-R14")]
     Hinge4BB_R14,
+
+    [Description("ОТLAV 30 х 120")]
     HingeOTLAV_30x120,
 }
